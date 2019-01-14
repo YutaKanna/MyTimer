ff56561447278d8060688ae734fcca4024e74263
YutaKanna <kannayuta0629@gmail.com>
2019-01-14 14:10:07 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 漢那優太 on 2019/01/14.
+//  Copyright © 2019 Yuta Kanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
