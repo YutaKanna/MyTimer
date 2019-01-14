ef760d30874c17f2cfaadd89dfdb11cc18b02a0b
YutaKanna <kannayuta0629@gmail.com>
2019-01-14 21:10:32 +0900

    PickerVIewを利用できるようにする

--- MyTimer/SettingViewController.swift
+++ MyTimer/SettingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
