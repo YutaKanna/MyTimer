0177341eff5f9ef11029d10d59379e287a650abd
YutaKanna <kannayuta0629@gmail.com>
2019-01-14 15:22:41 +0900

    設定画面用のプログラムを記述するSettingViewControllerを追加する

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  MyTimer
+//
+//  Created by 漢那優太 on 2019/01/14.
+//  Copyright © 2019 Yuta Kanna. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
